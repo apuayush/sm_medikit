55e02955c27b443f2be1cec1f84bb62c56c58159
Surya <surya.thiru001@gmail.com>
2018-03-12 03:58:47 +0530

    header

--- controllers/emergency.py
+++ controllers/emergency.py
@@ -8,7 +8,8 @@ class EmergencyHandler(RequestHandler):
     def set_default_headers(self):
         print("setting headers!!!")
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
 
     def options(self):
         # no body
