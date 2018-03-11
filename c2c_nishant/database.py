6fc16a751d1003f9268eb325f6f850813f86ac5a
Surya <surya.thiru001@gmail.com>
2018-03-11 05:30:32 +0530

    collobarative filtering model complete

--- c2c_nishant/database.py
+++ c2c_nishant/database.py
@@ -10,13 +10,13 @@ data = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 ans = []
-for _ in range(40):
+for _ in range(100):
     val = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     for i in np.random.choice(lst, 4):
         val[i] = 0
     ans.append(val)
 
-print ans[:2]
+print(ans[:2])
 with open("clustering_dataset.csv", 'w') as f:
     a = csv.writer(f, delimiter=',')
     a.writerows(ans)
